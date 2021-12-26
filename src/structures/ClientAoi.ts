42c5a32410913c47958019ca0c69cba6b58a3067
Leref <12578483+Leref@users.noreply.github.com>
2021-12-26 10:15:40 -0500

    clientAoi

---
+++ src/structures/ClientAoi.ts
@@ -0,0 +1,8 @@
+import {ApplicationCommandData, Client, Collection, Intents, IntentsString, Message} from "discord.js";
+
+export class ClientAoi {
+    /**
+     * The client managed by this Client.
+     */
+    client: Client
+}
