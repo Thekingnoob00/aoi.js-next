e113fd75b1b1f6e98fb8892307a3dbaf763e8fd6
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2021-12-27 20:01:00 +0100

    Refactored functions

--- src/typings/types/FunctionExecutor.ts
+++ src/typings/types/FunctionExecutor.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { ThisArg } from "../interfaces/ThisArg";
 import { Async } from "./Async";
-=======
-import {Async} from "./Async";
->>>>>>> 2885c34042a128338948636260a11fe138f91c47
 
 export type FunctionExecutor<T = unknown> = (this: ThisArg<T>) => Async<unknown>
